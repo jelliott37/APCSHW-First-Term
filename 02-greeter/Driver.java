cdec091ca8272b19e5dead2b7ef9c16df84bdf02
Jesse Elliott <jessee8991@gmail.com>
2014-09-15 08:39:04 -0400

    Added Driver.java

---
+++ 02-greeter/Driver.java
@@ -0,0 +1,16 @@
+public class Driver {
+
+    public static void main(String[] args){
+
+	// we are declaring g to be of type Greeter
+	Greeter g;
+	// local variable
+
+	g = new Greeter();
+
+	//call greet method on instance g
+	g.greet();
+
+	//g.ungreet();
+    }
+}
--- 02-greeter/Greeter.java
+++ 02-greeter/Greeter.java
@@ -2,8 +2,12 @@
 import java.io.*;
 import java.util.*;
 
-public class greeter{
+public class Greeter{
     public void greet(){
 	System.out.println("Hello, World!");//Fun comments and stuff
+	ungreet();
+    }
+    private void ungreet(){
+	System.out.println("Goodbye, World!");
     }
 }
