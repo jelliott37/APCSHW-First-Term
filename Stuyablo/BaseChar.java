67106e0bc419abba69ea38dd2426630bf9897146
Jesse Elliott <Jesse@kagutsuchi.home>
2014-10-16 21:11:49 -0400

    Start of Stuyablo

---
+++ Stuyablo/BaseChar.java
@@ -0,0 +1,61 @@
+public class BaseChar{
+    
+    private int hp,mp,atk,def,spd,acc,lck;
+    private bool poisonState=false;
+    private String name, weapon;
+
+    public BaseChar(int h, int m, int a, int d, int s, int ac, int c, String n){
+	hp= h;
+	mp=m;
+	atk=a;
+	def=d;
+	spd=s;
+	acc=ac;
+	lck=c;
+	name=n;
+	//I'll add some degree of randomness later.
+    }
+    public String toString(){
+	return name;
+    }
+    
+    public int getHP(){
+	return hp;
+    }
+    public void setHP(int h){
+        hp = h;
+    }
+    public int getAtk(){
+	return atk;
+    }
+    public int getDef(){
+	return def;
+    }
+    public int getSpd(){
+	return spd;
+    }
+    public int getLck(){
+	return lck;
+    }
+    public String getWeapon(){
+	return weapon;
+    }
+    public bool isPoisoned(){
+	return poison;
+    }
+    public String poison(){
+	if (!poisonState){
+	    poisonState = true;
+	    return "" + this + "was poisoned.";
+	}
+	return "" + this + "was already poisoned.";
+    }
+    public String attack(BaseChar other){
+	if (other.getDef()>this.getAtk()){
+	    return "" + this + " could deal no damage to " + other;
+	}
+	other.setHP(other.getHP()+other.getDef()-this.getAtk());
+	return "" + this + " attacked " + other + " with " + weapon + " and injured " + other;
+    }
+
+}
