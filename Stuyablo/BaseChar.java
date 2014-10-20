43a266ff1518bf7a0666adcb4d886876608d8f91
Jesse Elliott <jessee8991@gmail.com>
2014-10-20 08:42:13 -0400

    z

--- Stuyablo/BaseChar.java
+++ Stuyablo/BaseChar.java
@@ -1,21 +1,20 @@
+import java.util.Random;
 public class BaseChar{
 
     private int hp,mp,atk,def,spd,acc,lck;
     private boolean poisonState=false;
     private String name, weapon;
-
+    Random r = new Random();
 
     public BaseChar(int h, int m, int a, int d, int s, int ac, int c, String n){
-	     Random r = new Random();
-	hp= r.nextInt(2)+h+1;
-	mp=m;
-	atk=a;
-	def=d;
-	spd=s;
-	acc=ac;
-	lck=c;
+	hp= r.nextInt(2)+h-1;
+	mp=r.nextInt(2)+m-1;
+	atk=r.nextInt(2)+a-1;
+	def=r.nextInt(2)+d-1;
+	spd=r.nextInt(2)+s-1;
+	acc=r.nextInt(2)+ac-1;
+	lck=r.nextInt(2)+c-1;
 	name=n;
-	//I'll add some degree of randomness later.
     }
     public String toString(){
 	return name;
@@ -23,6 +22,12 @@ public class BaseChar{
     
     public int getHP(){
 	return hp;
+    }
+    public int getMP(){
+	return mp;
+    }   
+    public void setMP(int m){
+	mp=m;
     }
     public void setHP(int h){
         hp = h;
@@ -53,11 +58,18 @@ public class BaseChar{
 	return "" + this + "was already poisoned.";
     }
     public String attack(BaseChar other){
-	if (other.getDef()>this.getAtk()){
-	    return "" + this + " could deal no damage to " + other;
+	if (r.nextInt(100)+1<acc){
+	    if (other.getDef()>this.getAtk()){
+		return "" + this + " could deal no damage to " + other;
+	    }
+	    if (r.nextInt(100)+1<lck){
+		other.setHP(other.getHP()+2*(other.getDef()-this.getAtk()));
+		return "" + this + " attacked " + other + " with " + weapon + " and injured " + other;
+	    }
+	    other.setHP(other.getHP()+other.getDef()-this.getAtk());
+	    return "" + this + " attacked " + other + " with " + weapon + " and injured " + other;
 	}
-	other.setHP(other.getHP()+other.getDef()-this.getAtk());
-	return "" + this + " attacked " + other + " with " + weapon + " and injured " + other;
+	return "" + this + " missed " + other;
     }
 
 }
