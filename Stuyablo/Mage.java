78c3b9595c4c17785f9cc6cd3a45664b75759676
Jesse Elliott <Jesse@kagutsuchi.home>
2014-10-21 23:25:50 -0400

    s

--- Stuyablo/Mage.java
+++ Stuyablo/Mage.java
@@ -11,16 +11,16 @@ public class Mage extends BaseChar {
     }
     public String spell(Monster other, String userInput){
 	if(userInput.equals( "1") && this.getACC() > 20 ) {
-	    other.setHP(other.getHP() - 10);
+	    other.setHP(other.getHP() -  thing.nextInt(2)-10);
 	    this.setMP(this.getMP() - 20);
 	    return(this + " shot a fireball at " + other);
 	       
 	}else if(userInput.equals("2") && this.getACC() > 0) {
-	    other.setHP(other.getHP() - 10);
+	    other.setHP(other.getHP() -10);
 	    this.setMP(this.getMP() - 30);
 	    return(this + " electrified " + other);
 	}else if(userInput.equals("3") && this.getACC() > 20) {
-	    other.setHP(other.getHP() - 20);
+	    other.setHP(other.getHP() - thing.nextInt(3) - 19);
 	    this.setMP(this.getMP() - 40);
 	    return(this + " froze " + other);
 	} else{
