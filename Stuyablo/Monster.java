2ead41c2d675b3fd618deb91d94670508fd206f0
Jesse Elliott <jessee8991@gmail.com>
2014-10-22 07:59:25 -0400

    finished

--- Stuyablo/Monster.java
+++ Stuyablo/Monster.java
@@ -5,15 +5,15 @@ public class Monster extends BaseChar {
     Random thing = new Random();
 
     public Monster(int h, int m, int a, int d, int s, int ac, int c, String n, String w){
-	super(h,m,a,d,s,ac,c,n,w);
+	 super(h,m,a,d,s,ac,c,n,w);
     }
 
     public void claws(BaseChar other){
 	if (thing.nextInt(10) <= 8) {
-	    int damage = r.nextInt(2)+6;
-	    System.out.println(this + " swiped at " + other);
-	    System.out.println(other + " lost "+ damage+ " HP.");
-	}
+	    int damage = 10 + thing.nextInt(11);
+	    other.setHP(other.getHP() - damage);
+	    System.out.println(this + " swiped at " + other + ". " + other + " lost "+ damage+ " HP.");
+	    }
 	else {
 	    System.out.println("Claws did not reach target...");
 	}
@@ -21,24 +21,25 @@ public class Monster extends BaseChar {
 
     public void teeth(BaseChar other) {
 	if (thing.nextInt(10) < 8) {
-	    int damage = r.nextInt(2)+9;
-	    System.out.println(this + " savagely bit "+ other);
-	    System.out.println(other + " lost "+ damage+ " HP.");
-	}
+	    int damage = 15 + thing.nextInt(11);
+	    other.setHP(other.getHP() - damage);
+	    System.out.println(this + " savagely bit "+ other + ". " + other + " lost "+ damage+ " HP.");
+	    }
 	else {
 	    System.out.println(other + " was out of biting reach...");
 	}
     }
-    
+	
     public void wrath(BaseChar other) {
 	if (thing.nextInt(10) < 7) {
-	    int damage = r.nextInt(6)+9;
-	    System.out.println(this + " howled into the moonlight, summoniong a great evil upon "+ other);
-	    System.out.println(other + "lost "+ damage+ " HP.");
-	    if (thing.nextInt(2)== 1) {
-		System.out.println(other + " was poisoned!");
+	    int damage = 10 + thing.nextInt(11);
+	    other.setHP(other.getHP() - damage);
+	    System.out.println(this + " howled into the moonlight, summoniong a great evil upon " + other + ". " + other + " lost "+ damage+ " HP.");
+		if (thing.nextInt(2)== 1) {
+			System.out.println(other + " was poisoned!");
+			other.makePoisoned();
+		    }
 	    }
-	}
 	else {
 	    System.out.println(other+ " blinded " + this + " with their beauty... attack missed.");
 	}
