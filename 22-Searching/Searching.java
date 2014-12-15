80e5c7ed93cd22d5095cb319edd0c52183b4fc4a
Jesse Elliott <Jesse@kagutsuchi.home>
2014-12-14 23:06:53 -0500

    stuff

--- 22-Searching/Searching.java
+++ 22-Searching/Searching.java
@@ -1,32 +1,32 @@
 public class Searching{
-    private Comparable[] a;
+    private String[] a;
     private int last=0;
     public Searching(int b){
-	a= new Comparable[b];
+	a= new String[b];
     }
-    public void additem(Comparable item){
+    public void additem(String item){
 	if (last<a.length){
 	    a[last]= item;
 	    last++;
 	}
     }
-    public Comparable lsearch(Comparable b){
-	for (Comparable c: a){
+    public String lsearch(String b){
+	for (String c: a){
 	    if(c.compareTo(b)==0){
 		return c;
 	    }
 	}
 	return null;
     }
-    public Comparable bsearch(Comparable b){
+    public String bsearch(String b){
 	int low = 0;
 	int high = a.length;
 	while (low<high){
 	    int mid = (high + low)/2;
-	    if (a[mid].comparesTo(b)==0){
+	    if (a[mid].compareTo(b)==0){
 		return a[mid];
 	    }
-	    else if(a[mid].comparesTo(b)<0){
+	    else if(a[mid].compareTo(b)<0){
 		low=mid;
 	    }
 	    else{
@@ -35,19 +35,19 @@ public class Searching{
 	}
 	return null;
     }
-    public Comparable rbsearch(int low, int high, Comparable item){
+    public String rbsearch(int low, int high, String b){
 	int mid = (high+low)/2;
-	if (a[mid].comparesTo(b)==0){
+	if (a[mid].compareTo(b)==0){
 	    return a[mid];
 	}
 	else if (low>=high){
 	    return null;
 	}
-	else if (a[mid].comparesTo(b)<0){
-	    rbsearch(mid, high, item);
+	else if (a[mid].compareTo(b)<0){
+        return rbsearch(mid, high, b);
 	}
 	else {
-	    rbsearch(low, mid, item);
+        return rbsearch(low, mid, b);
 	}
     }
     public static void main(String[] args){
