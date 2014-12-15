7bbf54a0d015987defe308d8474c6ce077c191ca
Jesse Elliott <Jesse@kagutsuchi.home>
2014-12-14 23:08:26 -0500

    stuff

---
+++ 22-Searching/SearchingBase.java
@@ -0,0 +1,56 @@
+public class Searching{
+    private Comparable[] a;
+    private int last=0;
+    public Searching(int b){
+	a= new Comparable[b];
+    }
+    public void additem(Comparable item){
+	if (last<a.length){
+	    a[last]= item;
+	    last++;
+	}
+    }
+    public Comparable lsearch(Comparable b){
+	for (Comparable c: a){
+	    if(c.compareTo(b)==0){
+		return c;
+	    }
+	}
+	return null;
+    }
+    public Comparable bsearch(Comparable b){
+	int low = 0;
+	int high = a.length;
+	while (low<high){
+	    int mid = (high + low)/2;
+	    if (a[mid].compareTo(b)==0){
+		return a[mid];
+	    }
+	    else if(a[mid].compareTo(b)<0){
+		low=mid;
+	    }
+	    else{
+		high=mid;
+	    }
+	}
+	return null;
+    }
+    public Comparable rbsearch(int low, int high, Comparable b){
+	int mid = (high+low)/2;
+	if (a[mid].compareTo(b)==0){
+	    return a[mid];
+	}
+	else if (low>=high){
+	    return null;
+	}
+	else if (a[mid].compareTo(b)<0){
+        return rbsearch(mid, high, b);
+	}
+	else {
+        return rbsearch(low, mid, b);
+	}
+    }
+    public static void main(Comparable[] args){
+	
+    }
+}
